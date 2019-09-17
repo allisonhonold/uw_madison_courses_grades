db9a021372544fef1c79f83c4593de08073c91a0
Aaron W Chen <awchen2009@gmail.com>
2019-09-16 17:18:47 -0700

    Describe given data set, begin data exploration

---
+++ create_uw_madison_database.sh
@@ -0,0 +1,22 @@
+# make directory for data
+mkdir data
+
+# change into data directory
+cd data/
+
+# Install unzip if not on system, via homebrew
+brew install unzip
+
+# decompress the database files
+# assumes that the zip is in the data folder
+unzip uw-madison-courses.zip
+
+# create new psql database
+createdb uw_madison
+
+# trasnfer SQLite database to uw_madison PostgreSQL database
+pgloader database.sqlite postgresql:///uw_madison
+
+# make directory for created data
+
+mkdir write_data
