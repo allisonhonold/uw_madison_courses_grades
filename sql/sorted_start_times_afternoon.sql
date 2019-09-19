e5efe2442542b70846274fcd5fe534d5b9f465df
Aaron W Chen <awchen2009@gmail.com>
2019-09-19 14:26:27 -0700

    Prepare for merging and final presentations

--- sql/sorted_start_times_afternoon.sql
+++ sql/sorted_start_times_afternoon.sql
@@ -1,3 +1,10 @@
+/*
+Object:         Query
+Author:         Aaron Chen
+Script Date:    September 19, 2019
+Description:    Returns the classes that start after 12:00 noon.
+*/
+
 CREATE MATERIALIZED VIEW all_afternoon_classes AS
     SELECT * 
     FROM 
